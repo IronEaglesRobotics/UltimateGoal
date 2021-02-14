dc16878b5c96f91098eb4722fcab9a64223e2e21
Robby <robby@teals.org>
2021-02-14 11:21:04 -0600

    added steps for autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Step.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Step.java
@@ -1,23 +1,40 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import org.firstinspires.ftc.teamcode.opencv.Detection;
+
 // Class for every step that the autonomous program will take
 public abstract class Step {
-    private int timeout;
+    private final double timeout;
+
+    // variables when turning
+    public double degreesToTurn;
+    public double heading;
+
+    // variables when shooting
+    public int ringsFired;
+    public Detection powershot;
+    public double z;
+    public boolean aimedAtPowershots;
+    public boolean firing;
+    public boolean zig;
+    public double zigTime;
+    public double zagTime;
 
     // Constructors
     public Step() {
         this.timeout = -1;
     }
-    public Step(int timeout) {
+    public Step(double timeout) {
         this.timeout = timeout;
     }
 
-    // Abstract methods to be overrided
+    // Abstract methods to be overrode
     public abstract void start();
+    public abstract void whileRunning();
     public abstract boolean isActive();
 
     // Return the timeout for the step
-    public int getTimeout() {
+    public double getTimeout() {
         return timeout;
     }
 }
